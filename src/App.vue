cf3f97871590bddd157b64bbeebdc3d21a1749c5
davidtheweb-dev <davidhermosilladuro@gmail.com>
2023-07-25 11:25:12 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <section
+    class="flex h-screen items-center justify-center bg-zinc-700 text-2xl font-medium text-white"
+  >
+    <h1>Starter app for Vue + Vite + Tailwind (w/ ESlint & Prettier) projects</h1>
+  </section>
+</template>

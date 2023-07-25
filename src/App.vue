9cd1cbae67af832d6618ddb8e0c7f528966f06a5
davidtheweb.dev <davidhermosilladuro@gmail.com>
2023-07-25 13:11:16 +0200

    Small text change

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,6 @@
   <section
     class="flex h-screen items-center justify-center bg-zinc-700 text-2xl font-medium text-white"
   >
-    <h1>Starter app for Vue + Vite + Tailwind (w/ ESlint & Prettier) projects</h1>
+    <h1>Starter app for Vue + Vite + Tailwind projects (w/ ESlint & Prettier)</h1>
   </section>
 </template>
